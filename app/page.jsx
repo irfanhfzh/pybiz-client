ab26d3e09b3ccb633cd6b5131bfc95e0a5525be5
irfanhfzh <irfanhfzh284@gmail.com>
2025-03-19 16:58:27 +0700

    fixing user interfaces

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import {
@@ -17,6 +17,7 @@ import {
   Receipt,
   Settings,
   ShoppingBag,
+  X,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import LogoPYBIZSquare from "@/public/logos/logo-pybiz-square.svg";
@@ -27,7 +28,7 @@ import Voucher from "@/public/icons/voucher.svg";
 import Electricity from "@/public/icons/electricity.svg";
 import Dashboard from "@/public/icons/dashboard.svg";
 import BannerImage from "@/public/images/banner.webp";
-import LogoPYBIZWhiteTitle from "@/public/logos/logo-pybiz-white-title.svg";
+import LogoPYBIZWhiteTitleMini from "@/public/logos/logo-pybiz-white-title-mini.svg";
 
 const digitalProducts = [
   { name: "Mobile Prepaid", icon: Smartphone },
@@ -90,7 +91,11 @@ const features = [
 
 export default function Home() {
   const [selectedFeatures, setSelectedFeatures] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
   const [showTrxSettings, setShowTrxSettings] = useState(false);
+  const [showDigitalProducts, setShowDigitalProducts] = useState(false);
+  const modalTrsxSettingsRef = useRef(null);
+  const modalDigitalProductsRef = useRef(null);
 
   const toggleFeature = (featureId) => {
     setSelectedFeatures((prev) =>
@@ -100,20 +105,76 @@ export default function Home() {
     );
   };
 
+  const handleProductClick = (product) => {
+    setSelectedProduct(product.name);
+
+    // Tindakan sesuai dengan produk yang dipilih
+    switch (product.name) {
+      case "Mobile Prepaid":
+        console.log("Navigating to Mobile Prepaid page");
+        // Tambahkan kode navigasi atau tindakan lain di sini
+        // Contoh: navigate('/mobile-prepaid');
+        break;
+      case "Internet":
+        console.log("Navigating to Internet page");
+        // Contoh: navigate('/internet');
+        break;
+      case "Mobile PIN":
+        console.log("Navigating to Mobile PIN page");
+        // Contoh: navigate('/mobile-pin');
+        break;
+      case "Electricity":
+        console.log("Navigating to Electricity page");
+        // Contoh: navigate('/electricity');
+        break;
+      case "View All":
+        console.log("Viewing all digital products");
+        // Contoh: navigate('/digital-products');
+        setShowDigitalProducts(true);
+        break;
+      default:
+        console.log("Unknown product");
+    }
+  };
+
+  // Fungsi untuk menangani klik di luar komponen
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        modalTrsxSettingsRef.current &&
+        !modalTrsxSettingsRef.current.contains(event.target)
+      ) {
+        setShowTrxSettings(false);
+      } else if (
+        modalDigitalProductsRef.current &&
+        !modalDigitalProductsRef.current.contains(event.target)
+      ) {
+        setShowDigitalProducts(false);
+      }
+    };
+
+    // Tambahkan event listener ketika modal terbuka
+    if (showTrxSettings) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    // Cleanup event listener ketika komponen unmount atau modal tertutup
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showTrxSettings]);
+
   return (
     <>
       <div className="bg-gradient-primary mb-2 flex flex-col gap-4 rounded-br-xl rounded-bl-xl px-4 py-6">
         <div className="flex items-center justify-between">
-          <LogoPYBIZWhiteTitle className="w-1/2" />
-          <h1 className="text-lg font-semibold text-white">
-            Let&apos;s Start Transaction!
-          </h1>
-          <div className="mobile-md:gap-2 flex gap-1 text-white">
-            <button className="rounded-full p-1">
-              <Bell className="mobile-md:h-6 mobile-md:w-6 h-5 w-5" />
-            </button>
+          <LogoPYBIZWhiteTitleMini />
+          <div className="flex gap-2 text-white">
+            <Link href="/notifications" className="rounded-full p-1">
+              <Bell className="h-6 w-6" />
+            </Link>
             <Link href="/profile" className="rounded-full p-1">
-              <CircleUserRound className="mobile-md:h-6 mobile-md:w-6 h-5 w-5" />
+              <CircleUserRound className="h-6 w-6" />
             </Link>
           </div>
         </div>
@@ -141,7 +202,7 @@ export default function Home() {
         <div className="mb-4 flex items-center justify-between">
           <h2 className="text-primary font-semibold">Transaction Features</h2>
           <Settings
-            className="mobile-md:h-6 mobile-md:w-6 h-5 w-5"
+            className="h-6 w-6"
             onClick={() => setShowTrxSettings(true)}
           />
         </div>
@@ -296,23 +357,24 @@ export default function Home() {
         <h2 className="text-primary mb-4 font-semibold">
           Sell Digital Products Now
         </h2>
-        <div>
-          <div className="no-scrollbar flex gap-6 overflow-x-auto">
-            {digitalProducts.map((product) => {
-              const Icon = product.icon;
-              return (
-                <button
-                  key={product.name}
-                  className="flex max-w-[64px] flex-col items-center"
+        <div className="no-scrollbar flex gap-6 overflow-x-auto px-2">
+          {digitalProducts.map((product) => {
+            const Icon = product.icon;
+            return (
+              <button
+                key={product.name}
+                className="flex max-w-[64px] flex-col items-center"
+                onClick={() => handleProductClick(product)}
+              >
+                <div
+                  className={`my-2 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-purple-100 ${selectedProduct === product.name ? "ring-primary ring-2" : ""}`}
                 >
-                  <div className="mb-2 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-purple-100">
-                    <Icon />
-                  </div>
-                  <span className="text-xs">{product.name}</span>
-                </button>
-              );
-            })}
-          </div>
+                  <Icon />
+                </div>
+                <span className="text-xs">{product.name}</span>
+              </button>
+            );
+          })}
         </div>
       </div>
 
@@ -351,20 +413,30 @@ export default function Home() {
         </div>
       </div>
 
-      {showTrxSettings && (
-        <div className="fixed inset-0 bg-black/50 transition-opacity duration-300" />
-      )}
+      {/* modal features */}
+      {showTrxSettings ||
+        (showDigitalProducts && (
+          <div className="fixed inset-0 bg-black/50 transition-opacity duration-300" />
+        ))}
 
       <div
-        className={`bg-background fixed inset-x-0 bottom-0 z-40 transform rounded-t-3xl p-4 shadow-lg transition-transform duration-300 ease-in-out ${
+        className={`bg-background fixed inset-x-0 bottom-0 z-40 transform rounded-t-3xl px-4 pb-4 shadow-lg transition-transform duration-300 ease-in-out ${
           showTrxSettings ? "translate-y-0" : "translate-y-full"
         }`}
+        ref={modalTrsxSettingsRef}
       >
+        <button
+          className="ml-auto flex rounded-full pt-4 pb-1"
+          onClick={() => setShowTrxSettings(false)}
+        >
+          <X className="h-5 w-5 text-gray-500" />
+        </button>
         <div className="space-y-2">
+          {/* Close button di pojok kanan atas */}
           <h2 className="text-center text-lg font-semibold">
             Choose your transaction features
           </h2>
-          <p className="text-muted-foreground text-center text-sm">
+          <p className="text-center text-sm text-gray-500">
             At least one feature must be displayed on your homepage.
           </p>
           <div className="my-4 space-y-4 overflow-y-auto py-2">
@@ -404,9 +476,41 @@ export default function Home() {
             <Button
               variant="default"
               onClick={() => setShowTrxSettings(false)}
-              className="bg-primary text=white w-full"
+              className="bg-primary w-full"
             >
               Save
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`bg-background fixed inset-x-0 bottom-0 z-40 transform rounded-t-3xl px-4 pb-4 shadow-lg transition-transform duration-300 ease-in-out ${
+          showDigitalProducts ? "translate-y-0" : "translate-y-full"
+        }`}
+        ref={modalDigitalProductsRef}
+      >
+        <button
+          className="ml-auto flex rounded-full pt-4 pb-1"
+          onClick={() => setShowDigitalProducts(false)}
+        >
+          <X className="h-5 w-5 text-gray-500" />
+        </button>
+        <div className="space-y-2">
+          {/* Close button di pojok kanan atas */}
+          <h2 className="text-center text-lg font-semibold">
+            Let's sell digital products!
+          </h2>
+          <p className="text-center text-sm text-gray-500">
+            Enjoy the benefits of selling digital products without a deposit.
+          </p>
+          <div className="flex flex-col gap-2">
+            <Button
+              variant="default"
+              onClick={() => setShowDigitalProducts(false)}
+              className="bg-primary w-full"
+            >
+              Upgrade Your Plan
             </Button>
           </div>
         </div>
